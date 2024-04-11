a4a947227cd288b067b293e9cd15c143bc992f20
Cristhian Arostegui <cr.arostegui@gmail.com>
2024-04-10 20:33:15 -0400

    Add Command logs for friendly examples in console

--- behavioral-patterns/Command/CommandPattern.js
+++ behavioral-patterns/Command/CommandPattern.js
@@ -6,12 +6,16 @@ import SubstractCommand from './calculator/SubstractCommand';
 
 export default class CommandPattern extends Pattern {
   static run() {
+    console.log('---- Command ----')
     const calc = new Calculator();
     calc.showResult(); // 0
+    console.log('Execute AddCommand(4)');
     calc.executeCommand(new AddCommand(4)); // +4
     calc.showResult(); // 4
+    console.log('Execute AddCommand(6)');
     calc.executeCommand(new AddCommand(6)); // +6
     calc.showResult(); // 10
+    console.log('Execute SubstractCommand(4)');
     calc.executeCommand(new SubstractCommand(5)); // -5
     calc.showResult(); // 5
   }
